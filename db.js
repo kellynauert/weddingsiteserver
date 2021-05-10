8c9304a3b7655da9dbeb1cea4db2591c7394ffac
Kelly Nauert <kellynauert@gmail.com>
2021-05-09 22:24:17 -0400

    turn on dialect options

--- db.js
+++ db.js
@@ -8,12 +8,12 @@ const { Sequelize } = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   operatorsAliases: Sequelize.Op.Aliases,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false, // very important
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // very important
+    },
+  },
 });
 
 // Comment out line 10 to run locally
