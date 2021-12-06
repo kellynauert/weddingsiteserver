bbf1a6812b95a38cdd9caed5a324f66a8a240f89
kellynauert <74744118+kellynauert@users.noreply.github.com>
2021-12-05 23:08:41 -0500

    fix

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require('sequelize');
 
+let options = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+};
+
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   operatorsAliases: Sequelize.Op.Aliases,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false, // very important
-  //   },
-  // },
+  dialectOptions: process.env.PORT === '3000' ? null : options,
 });
 // Comment out lines 6-11 to run locally
 
