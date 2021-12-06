98e774d4b040c75e0a78e9d15dae539377fb7b91
kellynauert <74744118+kellynauert@users.noreply.github.com>
2021-12-05 23:24:02 -0500

    pls

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,12 +1,10 @@
 module.exports = function (req, res, next) {
-  process.env.PORT === '3000'
-    ? res.header('access-control-allow-origin', 'http://localhost:3001')
-    : res.header('access-control-allow-origin', 'www.steel-wedding.com');
-  res.header('access-control-allow-credentials', 'true');
+  res.header('access-control-allow-origin', '*');
   res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
   res.header(
     'access-control-allow-headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization, Options'
   );
+
   next();
 };
