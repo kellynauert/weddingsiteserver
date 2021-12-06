08730b29f9304f739654b7f021a1ac16f16ca252
kellynauert <74744118+kellynauert@users.noreply.github.com>
2021-12-05 23:16:21 -0500

    change

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,10 +1,15 @@
 module.exports = function (req, res, next) {
-  res.header('access-control-allow-origin', '*');
+  process.env.PORT === '3000'
+    ? res.header('access-control-allow-origin', 'http://localhost:3001')
+    : res.header(
+        'access-control-allow-origin',
+        'https://cre-data-collection.herokuapp.com'
+      );
+  res.header('access-control-allow-credentials', 'true');
   res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
   res.header(
     'access-control-allow-headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization, Options'
   );
-
   next();
 };
